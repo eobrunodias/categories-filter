d26204800be9dc50d2944a0267c40204202dc0e5
Bruno Dias <brunodias40@gmail.com>
2024-08-10 19:13:26 -0300

    feat: create theme and add types

---
+++ src/styles/themes/defaultTheme.tsx
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  colors: {
+    "gray-500": "#4f5675",
+    "gray-400": "#606787",
+    "gray-300": "#727899",
+    "gray-200": "#8389aa",
+    "gray-100": "#949abc",
+
+    "purple-500": "#370b89",
+    "purple-400": "#5122a2",
+    "purple-300": "#6b3abb",
+    "purple-200": "#8451d4",
+    "purple-100": "#9e68ed",
+  },
+};
